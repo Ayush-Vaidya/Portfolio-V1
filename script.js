eef7c05cc1642767d0896e2f9ec62b99f4ff4a17
Ayush Vaidya <83414737+Ayush-Vaidya@users.noreply.github.com>
2026-09-10 22:41:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-const cursor = document.getElementById('cursor');
-document.addEventListener('mousemove', e => {
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
-});
+
